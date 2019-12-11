320d5f9a32a0215fc1269c24c98d405da93e5928
w-zm <354242964@qq.com>
2019-12-11 15:13:15 +0800

    sif

--- README_zh.md
+++ README_zh.md
@@ -1,15 +1,35 @@
 # sentence2vec
 一个将句子转化为向量表征的工具库，并集成一些常用的算法。参考sklearn库的用法，尽可能地做到简单使用，后续会持续更新。
 
+输入：句子组成的list，如：['I like natural language processing', ..., 'This is an example']
+
+输出：[[0.1, 0.1, ..., 0.1], ..., [0.1, 0.1, ..., 0.1]]
+
 ## 依赖
-gensim 3.6.0
+- python 3.6
+
+- gensim 3.6.0
+
+- scikit-learn 0.21.2
 
 上述版本号仅为参考。
 
 ## 当前实现
-SIF算法{1}
+
+Model | Year | Status 
+:-: | :-: | :-: 
+SIF<sup>[1]</sup> (smooth inverse frequency) | 2016 | in progress 
+CPM<sup>[2]</sup> (concatenated power mean) | 2018 | plan 
+
+## 实例
 
 
-## 参考文献
+
+
+
+## Reference
+
 [1] Arora S, Liang Y, Ma T. A simple but tough-to-beat baseline for sentence embeddings[J]. 2016.
+
+[2] Rücklé A, Eger S, Peyrard M, et al. Concatenated power mean word embeddings as universal cross-lingual sentence representations[J]. arXiv preprint arXiv:1803.01400, 2018.
 
--- sentence2vec/SIF.py
+++ sentence2vec/SIF.py
@@ -1,44 +1,60 @@
 """smooth inverse frequency (SIF).
 """
-from gensim.models import KeyedVectors
+import numpy as np
 
-class SIF():
-    def __init__(self, word_file, weight_file, weight_para, data, rmpc=1):
+from gensim.models import KeyedVectors
+from sklearn.decomposition import TruncatedSVD
+
+class Params(object):
+    def __init__(self):
+        self.LW = 1e-5
+        self.LC = 1e-5
+        self.eta = 0.05
+
+    def __str__(self):
+        t = "LW", self.LW, ", LC", self.LC, ", eta", self.eta
+        t = list(map(str, t))
+        return ' '.join(t)
+
+class SIF(object):
+    def __init__(self, word_file, weight_file, sentences, weight_para=1e-3, rmpc=1):
         self.word_file = word_file
         self.weight_file = weight_file
-        self.weight_para = weight_para
-        self.data = data
+        self.sentences = sentences
+        self.weight_para = weight_para if weight_para > 0 else 1.0     # when the parameter makes no sense, use unweighted
         self.rmpc = rmpc
 
         print('--- load word vectors ---')
-        (words, We) = self.getWordmap(word_file)
+        self.words, self.We = self.getWordmap()
         print('--- finish load word vectors ---')
         print('--- load word weights ---')
-        word2weight = self.getWordWeight(self.weight_file, self.weight_para)  # word2weight['str'] is the weight for the word 'str'
+        self.word2weight = self.getWordWeight()  # word2weight['str'] is the weight for the word 'str'
         print('--- finish load word weights ---')
         print('--- start weight4ind ---')
-        weight4ind = self.getWeight(words, word2weight)  # weight4ind[i] is the weight for the i-th word
+        self.weight4ind = self.getWeight()  # weight4ind[i] is the weight for the i-th word
         print('--- finish weight4ind ---')
 
-    # def transform(self):
-    #     x, m = self.sentences2idx(self.data, words)  # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
-    #     w = seq2weight(x, m, weight4ind)  # get word weights
-    #     embedding = SIF_embedding(We, x, w, params)
+    def transform(self):
+        params = Params()
+        params.rmpc = self.rmpc
 
-    def getWordmap(self, word_file):
-        w2v_model = KeyedVectors.load_word2vec_format(word_file)
+        x, m = self.sentences2idx(self.sentences, words)  # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
+        w = self.seq2weight(x, m)  # get word weights
+        embedding = self.SIF_embedding(x, w, params)
+
+        return embedding
+
+    def getWordmap(self):
+        w2v_model = KeyedVectors.load_word2vec_format(self.word_file)
         word2ids = w2v_model.wv.vocab
         words = {}
         for word, vocab in word2ids.items():
             words[word] = vocab.index
-        return (words, w2v_model.vectors)
+        return words, w2v_model.vectors
 
-    def getWordWeight(self, weight_file, a=1e-3):
-        if a <= 0:  # when the parameter makes no sense, use unweighted
-            a = 1.0
-
+    def getWordWeight(self):
         word2weight = {}
-        with open(weight_file, encoding='utf-8') as f:
+        with open(self.weight_file, encoding='utf-8') as f:
             lines = f.readlines()
         N = 0
         for i in lines:
@@ -55,14 +71,14 @@ class SIF():
                 print('format error')
                 exit()
         for key, value in word2weight.items():
-            word2weight[key] = a / (a + value / N)
+            word2weight[key] = self.weight_para / (self.weight_para + value / N)
         return word2weight
 
-    def getWeight(self, words, word2weight):
+    def getWeight(self):
         weight4ind = {}
-        for word, ind in words.items():
-            if word in word2weight:
-                weight4ind[ind] = word2weight[word]
+        for word, ind in self.words.items():
+            if word in self.word2weight:
+                weight4ind[ind] = self.word2weight[word]
             else:
                 weight4ind[ind] = 1.0
         return weight4ind
@@ -96,9 +112,9 @@ class SIF():
         x_mask = np.asarray(x_mask, dtype='float32')
         return x, x_mask
 
-    def seq2weight(self, seq, mask, weight4ind):
-        weight4ind_np = np.zeros((len(weight4ind),))
-        for index, weight in weight4ind.items():
+    def seq2weight(self, seq, mask):
+        weight4ind_np = np.zeros((len(self.weight4ind),))
+        for index, weight in self.weight4ind.items():
             weight4ind_np[index] = weight
 
         weight = np.zeros(seq.shape).astype('float32')
@@ -106,3 +122,56 @@ class SIF():
             weight[index] = weight4ind_np[row]
         weight = weight * mask
         return weight
+
+    def SIF_embedding(self, x, w, params):
+        """
+        Compute the scores between pairs of sentences using weighted average + removing the projection on the first principal component
+        :param We: We[i,:] is the vector for word i
+        :param x: x[i, :] are the indices of the words in the i-th sentence
+        :param w: w[i, :] are the weights for the words in the i-th sentence
+        :param params.rmpc: if >0, remove the projections of the sentence embeddings to their first principal component
+        :return: emb, emb[i, :] is the embedding for sentence i
+        """
+        emb = self.get_weighted_average(x, w)
+        if params.rmpc > 0:
+            emb = self.remove_pc(emb, params.rmpc)
+        return emb
+
+    def get_weighted_average(self, x, w):
+        """
+        Compute the weighted average vectors
+        :param We: We[i,:] is the vector for word i
+        :param x: x[i, :] are the indices of the words in sentence i
+        :param w: w[i, :] are the weights for the words in sentence i
+        :return: emb[i, :] are the weighted average vector for sentence i
+        """
+        n_samples = x.shape[0]
+        emb = np.zeros((n_samples, self.We.shape[1]))
+        for i in range(n_samples):
+            emb[i, :] = w[i, :].dot(self.We[x[i, :], :]) / np.count_nonzero(w[i, :])
+        return emb
+
+    def remove_pc(self, X, npc=1):
+        """
+        Remove the projection on the principal components
+        :param X: X[i,:] is a data point
+        :param npc: number of principal components to remove
+        :return: XX[i, :] is the data point after removing its projection
+        """
+        pc = self.compute_pc(X, npc)
+        if npc == 1:
+            XX = X - X.dot(pc.transpose()) * pc
+        else:
+            XX = X - X.dot(pc.transpose()).dot(pc)
+        return XX
+
+    def compute_pc(self, X, npc=1):
+        """
+        Compute the principal components. DO NOT MAKE THE DATA ZERO MEAN!
+        :param X: X[i,:] is a data point
+        :param npc: number of principal components to remove
+        :return: component_[i,:] is the i-th pc
+        """
+        svd = TruncatedSVD(n_components=npc, n_iter=7, random_state=0)
+        svd.fit(X)
+        return svd.components_
